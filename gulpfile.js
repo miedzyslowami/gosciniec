8c1bbcd3851de4856ea059d17a85f1914e2f3a63
Małgorzata Grajewska <malgorzata.grajewska@hotmail.co.kr>
2017-05-22 20:20:51 +0200

    Autoprefixer installed

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var autoprefixer=require('gulp-autoprefixer');
 
 gulp.task('sass', function() {
     return gulp.src('scss/main.scss')
@@ -10,6 +11,7 @@ gulp.task('sass', function() {
             outputStyle: 'expanded'
         }))
         .pipe(sourcemaps.write())
+        .pipe(autoprefixer())
         .pipe(gulp.dest('css'))
 });
 
